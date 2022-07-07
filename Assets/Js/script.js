9109364a1b82cc13ef32bdee10deab3ea89eac7f
Grady253 <grady.andre2@gmail.com>
2022-07-07 15:32:01 -0700

    I changed my timeblock lay out. I added some variables and started my for loop on javascript.

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,2 +1,26 @@
-var today = moment();//This is deplaying the current dat.
-$("#currentDay").text(today.format("[Today's date is:] MMMM Do, YYYY"));
+//These are the variables I'll be calling in my code below.
+
+var past = $("#past");
+var present =$("#present");
+var future = $("#future");
+var today = moment();//This is displaying the current day.
+
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+//This for loop is to count the time
+for(var i =9; i <=18; i++){
+    if (i > today){
+        document.getElementById(".col-md-10") = past;
+    };
+    
+    if (i === today){
+        document.getElementById(".col-md-10") = present;
+    };
+
+    if(i < today){
+        document.getElementById(".col-md-10") = future;
+    };
+
+
+
+}
