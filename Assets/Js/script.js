3ea553f815cb4b6450c186fe1fcfa9dace773a22
Grady253 <grady.andre2@gmail.com>
2022-08-16 07:41:07 -0700

    I corrected my if/else statment to function properly

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,42 +1,39 @@
-//setting a const declaring to the date.
-const today = moment(); //This is displaying the current day.
+const today = moment(); // display the current time.
+const currentHour = moment().hour(); // Displaying the current hour.
+console.log(currentHour);
+const textBox = $(".description");
+console.log(textBox);
+const saveButton = $(".saveBtn");
+const timeBox = textBox;
+console.log(timeBox);
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY!")); // Displayng the date.
 
-const hour = moment().hour(); //Determines the hour 
 
-$("#currentDay").text(today.format("dddd, MMMM Do YYYY!"));
+for (let i = 0; i < textBox.length ; i++) {
+  console.log(textBox[i].dataset.hour);
 
-function timecycle(){
-  for (let i = 9; i <= 17; i++) {
-
-    const info = "";
-
-    const key = "hour-"+i;
-
-    const template = ` <div id="hour-" class="row time-block">
-    <div class="col-md-1 hour">${i}</div>
-    <textarea class="col-md-10 description past present future">${info}</textarea>
-    <button type="button" class="saveBtn">${i}</button>
-    </div>`;
-
-    // if (i < hour){
-    //   $('row time-block').addClass('past');
-    // } else if (i == hour){
-    //   $('row time-block').addClass('present');
-    // } else{
-    //   $('row time-block').addClass('future');
-    //   console.log("it works");
-    // };
+  if (textBox[i].dataset.hour < currentHour){
+    textBox[i].classList.add('past');
+    console.log("past Hour");
+  } else if (textBox[i].dataset.hour == currentHour) {
+    textBox[i].classList.add('present');
+    console.log("Current Hour");
+  } else {
+    textBox[i].classList.add('future');
+    console.log("Future Hour");
   }
 
 };
 
-// function colorChange(){
-// if (i < hour){
-//   $('row time-block').addClass('past');
-// } else if (i === hour){
-//   $('row time-block').addClass('present');
-// } else{
-//   $('row time-block').addClass('future');
-//   console.log("it works");
-// };
-// };
+
+// function saveDisplay(){
+//   const key = "hour-" + i;
+//   const data ='';
+
+//   const template = `<div id="hour-" class="row time-block">
+//   <div class="col-md-1 hour">${key}</div>
+//   <textarea class="col-md-10 description past present future">${data}</textarea>
+//   <button type="button" class="saveBtn">${key}</button>
+//  </div>`
+
+// }
