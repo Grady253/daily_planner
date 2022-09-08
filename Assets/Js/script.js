ea59638b483976bed65c65847a723a336c01bdff
Grady253 <grady.andre2@gmail.com>
2022-09-08 15:20:04 -0700

    fixed my for loop so textbox change color as time passes.

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -2,38 +2,57 @@ const today = moment(); // display the current time.
 const currentHour = moment().hour(); // Displaying the current hour.
 console.log(currentHour);
 const textBox = $(".description");
-console.log(textBox);
 const saveButton = $(".saveBtn");
-const timeBox = textBox;
-console.log(timeBox);
+let containEl = $(".container");
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY!")); // Displayng the date.
 
+const input = localStorage.getItem("data");
 
-for (let i = 0; i < textBox.length ; i++) {
+for (let i = 0; i < textBox.length; i++) {
   console.log(textBox[i].dataset.hour);
 
-  if (textBox[i].dataset.hour < currentHour){
-    textBox[i].classList.add('past');
+  if (textBox[i].dataset.hour < currentHour) {
+    textBox[i].classList.add("past");
     console.log("past Hour");
   } else if (textBox[i].dataset.hour == currentHour) {
-    textBox[i].classList.add('present');
+    textBox[i].classList.add("present");
     console.log("Current Hour");
   } else {
-    textBox[i].classList.add('future');
+    textBox[i].classList.add("future");
     console.log("Future Hour");
   }
+}
 
-};
-
-
-// function saveDisplay(){
-//   const key = "hour-" + i;
-//   const data ='';
+// function saveDisplay() {
+//   let key = textBox;
+//   let data = ""; 
 
 //   const template = `<div id="hour-" class="row time-block">
-//   <div class="col-md-1 hour">${key}</div>
-//   <textarea class="col-md-10 description past present future">${data}</textarea>
-//   <button type="button" class="saveBtn">${key}</button>
-//  </div>`
+//   <div class="col-md-1 hour"></div>
+//   <textarea data-hour=${key}class="col-md-10 description">${data}</textarea>
+//   <button data-hour=${key} class="saveBtn"></button>
+//  </div>`;
 
+//  localStorage.setItem(key,input);
+ 
 // }
+
+containEl.addEventListener("click",(event) => {
+  
+  saveDisplay();
+
+});
+
+
+for (let i = 9; i < 17; i++) {
+  let key = "hour-" + i;
+  let data = ""; 
+
+  const template = `<div id="hour-" class="row time-block">
+  <div class="col-md-1 hour"></div>
+  <textarea data-hour=${i}class="col-md-10 description">${data}</textarea>
+  <button data-hour=${i} class="saveBtn"></button>
+ </div>`;
+
+ localStorage.setItem(key,input);
+}
